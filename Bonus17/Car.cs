d964a63ad5ad9f32613b35d77d11e2837338afc5
famouschamus <chamusg@gmail.com>
2018-02-05 13:25:38 -0500

    initial commit

---
+++ Bonus17/Car.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Bonus17
+{
+    class Car
+    {
+        public string make;
+        public string model;
+        public int year;
+        public double price;
+        public string carInfo;
+
+        public Car()
+        {
+            this.make = makeMake();
+            this.model = makeModel();
+            this.year = makeYear();
+            this.price = makePrice();
+            this.carInfo = make + " " + model + "   " + year + "    " + price;
+            Console.WriteLine(carInfo);
+        }
+
+        public static string makeMake()
+        {
+            Console.WriteLine("please enter the make of the car");
+            string methodMake = Console.ReadLine().ToLower();
+            return methodMake;
+        }
+        public static string makeModel()
+        {
+            Console.WriteLine("please enter the model of the car");
+            string methodModel = Console.ReadLine().ToLower();
+            return methodModel;
+        }
+        public static int makeYear()
+        {
+            Console.WriteLine("please enter the Year of the car");
+            bool works = int.TryParse(Console.ReadLine(), out int methodYear);
+            if (works = false || methodYear < 1400 || methodYear > 9999)
+            {
+                Console.WriteLine("Please enter a proper Year");
+                makeYear();
+            }
+
+            return methodYear;
+
+        }
+        public static double makePrice()
+        {
+            Console.WriteLine("please enter the Price of the car");
+            bool works = double.TryParse(Console.ReadLine(), out double methodPrice);
+            if (works = false || methodPrice < 0)
+            {
+                Console.WriteLine("Please enter a proper Price");
+                makePrice();
+            }
+
+            return methodPrice;
+
+
+        }
+    }
+}
---
+++ Bonus17/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bonus17
+{
+    class Program
+    {
+        static void Main()
+        {
+
+
+
+            Console.WriteLine("Hello, How many cars would you like to submit?");
+            bool works = int.TryParse(Console.ReadLine(), out int howMany);
+            if (works == false)
+            {
+                Console.WriteLine("Try again");
+                Main();
+            }
+            Car[] myCars = new Car[howMany];
+            List<Car> ListofCars = new List<Car>();
+
+            for (int i = 0; i < howMany; i++)
+            {
+                myCars[i] = new Car();
+                ListofCars.Add(myCars[i]);
+            }
+
+            foreach (Car item in ListofCars)
+            {
+                Console.WriteLine(item.carInfo);
+            }
+
+        }
+    }
+}
